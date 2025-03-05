18829a2a05098c58f4763173702d31145c52ff8e
schotts-misc <schotts@proton.me>
2025-03-05 06:14:27 +0000

    tty ready

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 STOWFARM=$HOME/.local/stow
 
-stow -n --dotfile -d $STOWFARM -t $HOME/ zsh
-stow -n -d $STOWFARM -t $HOME/.config/nvim nvim
-stow -d $STOWFARM -t $HOME/.config/hypr hypr
+stow --dotfile -d $STOWFARM -t $HOME/ zsh
+stow -d $STOWFARM -t $HOME/.config/nvim nvim
+stow -d $STOWFARM -t $HOME/.config/tmux tmux
--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -12,18 +12,19 @@ vim.opt.smartcase = true
 vim.opt.hlsearch = false
 
 -- Apperance
-vim.opt.termguicolors = true
+vim.opt.termguicolors = false
 vim.opt.relativenumber = true
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.colorcolumn = '80'
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.cmd('colorscheme quiet')
+vim.cmd('colorscheme pablo')
 
 -- Behaviour
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
+vim.opt.mouse = ''
 vim.opt.errorbells = false
 vim.opt.swapfile = true
 vim.opt.backupdir = '/tmp,.'
