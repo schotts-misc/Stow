3f232555a5efc68b60ea9ee7260b89497035f7a9
schotts-misc <schotts@proton.me>
2025-03-05 09:21:11 +0000

    MOOOOOORE

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ STOWFARM=$HOME/.local/stow
 
 stow --dotfile -d $STOWFARM -t $HOME/ zsh
 stow --dotfile -d $STOWFARM -t $HOME/ x
+stow --dotfile -d $STOWFARM -t $HOME/ links
 stow -d $STOWFARM -t $HOME/.config/kitty kitty
 stow -d $STOWFARM -t $HOME/.config/nvim nvim
 stow -d $STOWFARM -t $HOME/.config/tmux tmux
+stow -d $STOWFARM -t $HOME/.config/picom picom
